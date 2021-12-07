2cfd4fe3de6fe15b125c1d25dff2d2b0501531b5
KishanSensussoft <kishan.sensussoft@gmail.com>
2021-12-07 15:34:01 +0530

    version -11

--- app/src/main/java/com/kishan/imagepicker/MainActivity.kt
+++ app/src/main/java/com/kishan/imagepicker/MainActivity.kt
@@ -2,8 +2,11 @@ package com.kishan.imagepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
